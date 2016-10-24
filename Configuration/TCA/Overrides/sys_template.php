02ec735fbbce730a413a6b8d4b7710bbbaaa0515
Vlad Stawizki <vlad.stawizki@gmail.com>
2016-10-24 22:08:41 +0200

    Add DataProcessor for Gallery

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -1,7 +1,6 @@
 <?php
-// Add an entry in the static template list found in sys_templates for static TS
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
    'vs_typo3_gallery',
    'Configuration/TypoScript',
-   'Static Template'
+   'Image Gallery'
 );
